8882e2235449b1af570846f64e382caf838b2c6b
MilanGit12 <milpatel@paloaltonetworks.com>
2023-02-07 15:58:29 -0600

    Checking if yor updates based on comments added

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -53,6 +53,7 @@ resource "aws_s3_bucket" "financials" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
+  # checking to see if comments update yor tags in financials bucket
   bucket        = "${local.resource_prefix.value}-financials"
   acl           = "private"
   force_destroy = true
